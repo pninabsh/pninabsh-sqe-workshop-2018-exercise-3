be8736d01335f6bff213395a106f4bb42c2cdb48
pninabsh <pninabsh@gmail.com>
2018-12-28 12:31:54 +0200

    Added a split circle in the graph

--- src/js/nodesHandling.js
+++ src/js/nodesHandling.js
@@ -13,23 +13,24 @@ function deleteUnusedNodes(cfg){
 
 function updatePrevNode(cfgElement, nextElement){
     for(let prevElement of cfgElement.prev) {
-        if(prevElement.normal){
+        if(prevElement.normal === nextElement){
             prevElement.normal = nextElement.normal;
         }
-        else if(prevElement.true){
+        else if(prevElement.true === nextElement){
             prevElement.true = nextElement.normal;
         }
-        else if(prevElement.false){
+        else if(prevElement.false === nextElement){
             prevElement.false = nextElement.normal;
         }
     }
+
 }
 
 function mergeNodes(cfg){
     let newCfg = [];
     let minIndex = cfg.length;
     for(let i=0;i<cfg.length; i++){
-        if(cfg[i].normal && cfg[i].normal.prev.length === 1 && cfg[i].normal.normal){
+        if(cfg[i].normal && cfg[i].normal.prev.length === 1 && cfg[i].normal.normal && (cfg[i].parent === cfg[i].normal.parent || !cfg[i].parent)){
             if(i < minIndex){
                 minIndex = i;
             }
@@ -42,50 +43,6 @@ function mergeNodes(cfg){
         }
     }
     return newCfg;
-}
-
-function handleNodeLine(line, conditions){
-    let splittedLineArray = line.split(/\s\[/);
-    let nodeName = splittedLineArray[0];
-    let splittenLabel = splittedLineArray[1].split(/="/);
-    let nodeValue = splittenLabel[1];
-    let nodeType = conditions.includes(nodeName)?  'condition' : 'operation';
-    return nodeName + '=>' + nodeType + ': ' + nodeValue.substring(0, nodeValue.length - 1) + '\n';
-}
-
-function hanleTransitions(line, conditions){
-    let splittenTransitionArray = line.split(/\s->\s/);
-    let firstNode = splittenTransitionArray[0];
-    let splitedSecondNodeAray = splittenTransitionArray[1].split(/\s/);
-    let secondNode = splitedSecondNodeAray[0];
-    let splittedLabel = splitedSecondNodeAray[1].split(/=/);
-    let conditionType = splittedLabel[1];
-    let condition = '';
-    if(conditionType){
-        conditionType === '"true"' ? condition = '(yes)' : condition = '(no)';
-        if(!conditions.includes(firstNode)){
-            conditions.push(firstNode);
-        }
-    }
-    return firstNode + condition +  '->'+ secondNode + '\n';
-}
-
-export function formCfgGraph (cfgDotResult) {
-    let nodesString = '';
-    let transitionString = '';
-    let conditions = [];
-    let lines = cfgDotResult.split(']\n');
-    for(let line of lines){
-        if(line.indexOf('->') > -1 && line !== ''){
-            transitionString += hanleTransitions(line, conditions);
-        }
-    }
-    for(let line of lines){
-        if(line.indexOf('->') == -1 && line !== ''){
-            nodesString += handleNodeLine(line, conditions);
-        }
-    }
-    return nodesString + '\n' + transitionString;
 }
 
 export function cleanCFGGraph(cfg){
